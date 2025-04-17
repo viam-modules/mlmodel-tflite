c6946d2443929468d1844a1aaf5541065a45f630
Andrew Morrow <andrew.morrow@viam.com>
2025-04-17 14:26:23 -0400

    fixup to use ascii encoding

--- bin/package.ps1
+++ bin/package.ps1
@@ -6,7 +6,7 @@ refreshenv
 # Fixup `meta.json` to have `.exe at the end
 Copy-Item meta.json meta.json.backup
 
-jq -f .\bin\fixup_windows_entrypoint.jq meta.json.backup | Out-File -FilePath meta.json -Encoding utf8
+jq -f .\bin\fixup_windows_entrypoint.jq meta.json.backup | Out-File -FilePath meta.json -Encoding ASCII
 
 7z a -ttar module.tar $args
 7z a -tgzip -sdel module.tar.gz module.tar
