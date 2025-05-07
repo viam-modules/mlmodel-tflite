d0fa14575c1597659ccf17db58a73140b85a1459
Alan Davidson <alan@viam.com>
2025-05-07 13:44:41 -0400

    use a recent version of cmake

--- bin/build_bullseye.sh
+++ bin/build_bullseye.sh
@@ -19,8 +19,23 @@ apt upgrade -y
 # Debian Bullseye is old enough that the default python interpreter is Python2, but we're using
 # Python3 features. So, install python3, and then make a copy of it called just python. Don't care
 # if we mess up /bin: the entire container is going to be thrown out anyway.
-apt install -y cmake curl git libstdc++6 make python3 python3-pip
+apt install -y curl git libstdc++6 make python3 python3-pip
 cp $(which python3) /bin/python
+
+# Compiling the C++ SDK requires cmake version 3.25 or later, but Debian Bullseye only provides
+# cmake 3.18 or earlier. So, we need to build it from scratch.
+apt install -y wget build-essential checkinstall zlib1g-dev libssl-dev
+mkdir cmake
+pushd cmake
+# This is the version used in Ubuntu 24.04 as of May 2025.
+wget https://github.com/Kitware/CMake/releases/download/v3.30.2/cmake-3.30.2.tar.gz
+tar xzvf cmake-3.30.2.tar.gz
+cd cmake-3.30.2
+./bootstrap
+make
+make install
+popd # cmake-3.30.2
+popd # cmake
 
 # Boost is installed via b2, but the pre-compiled versions of b2 require a more recent version of
 # glibcxx than is available on Debian Bullseye. So, instead, build it ourselves. and that means
