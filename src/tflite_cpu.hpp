ddca157ff0c31661aabe850318ccf888f4e661c3
Lillian Hwang-Geddes <lillian.hwang-geddes@viam.com>
2026-01-06 11:42:10 -0500

    fix header file problems

--- src/tflite_cpu.hpp
+++ src/tflite_cpu.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <tensorflow/lite/c/c_api.h>
+#include <tensorflow/lite/interpreter_builder.h>
+#include <tensorflow/lite/kernels/register.h>
+
 #include <memory>
 #include <viam/sdk/config/resource.hpp>
 #include <viam/sdk/resource/resource.hpp>
+#include <viam/sdk/services/mlmodel.hpp>
+#include <viam/sdk/resource/reconfigurable.hpp>
+#include <viam/sdk/resource/stoppable.hpp>
 
-class MLModelServiceTFLite
+namespace vsdk = ::viam::sdk;
+
+class MLModelServiceTFLite : public vsdk::MLModelService,
+                             public vsdk::Stoppable,
+                             public vsdk::Reconfigurable
 {
 
 public:
@@ -16,41 +27,41 @@ public:
 
     ~MLModelServiceTFLite() final;
 
-void stop(const vsdk::ProtoStruct &extra) noexcept final
+    void stop(const vsdk::ProtoStruct &extra) noexcept final;
 
-    void stop() noexcept
+    void stop() noexcept;
 
     void reconfigure(const vsdk::Dependencies &dependencies,
-                     const vsdk::ResourceConfig &configuration) final
+                     const vsdk::ResourceConfig &configuration) final;
 
     std::shared_ptr<named_tensor_views> infer(const named_tensor_views &inputs,
-                                              const vsdk::ProtoStruct &extra) final
+                                              const vsdk::ProtoStruct &extra) final;
 
-    struct metadata metadata(const vsdk::ProtoStruct &extra) final
+    struct metadata metadata(const vsdk::ProtoStruct &extra) final;
 
-    private :
-
-    void check_stopped_inlock_() const
-
-    static std::unique_ptr<struct state_> configure_(vsdk::Dependencies dependencies,
-                                                     vsdk::ResourceConfig configuration)
+private:
+    void check_stopped_inlock_() const;
 
     // Converts from tflites type enumeration into the model service
     // type enumeration or throws if there is no such conversion.
     static MLModelService::tensor_info::data_types service_data_type_from_tflite_data_type_(
-        TfLiteType type)
+        TfLiteType type);
 
     // All of the meaningful internal state of the service is held in
     // a separate state object to help ensure clean replacement of our
     // internals during reconfiguration.
-    struct state_ final : public tflite::ErrorReporter;
+    // struct state_ final : public tflite::ErrorReporter;
 
     // A visitor that can populate a TFLiteTensor given a MLModelService::tensor_view.
-class write_to_tflite_tensor_visitor_ : public boost::static_visitor<TfLiteStatus> public :
+    class write_to_tflite_tensor_visitor_ : public boost::static_visitor<TfLiteStatus>
+    {
+    public:
+        write_to_tflite_tensor_visitor_(const std::string *name, TfLiteTensor *tflite_tensor);
 
-    TfLiteStatus
-    operator()(const T &mlmodel_tensor) const
-    // TODO not sure how internal classes work with header files
+        template <typename T>
+        TfLiteStatus
+        operator()(const T &mlmodel_tensor) const;
+    };
 
     // Creates a tensor_view which views a tflite tensor buffer. It dispatches on the
     // type and delegates to the templated version below.
