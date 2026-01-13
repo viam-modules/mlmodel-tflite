72fea8023af3c7e3182ec7bddacaec3ac31d5ce3
Lillian Hwang-Geddes <lillian.hwang-geddes@viam.com>
2026-01-13 16:10:07 -0500

    fix formatting with clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -7,64 +7,55 @@
 // TODO import tflite_cpu library
 #include "tflite_cpu.hpp"
 
-namespace
-{
+namespace {
 
-    int serve(const std::string &socket_path)
-    try
-    {
-        // Every Viam C++ SDK program must have one and only one Instance object which is created before
-        // any other C++ SDK objects and stays alive until all Viam C++ SDK objects are destroyed.
-        viam::sdk::Instance inst;
+int serve(const std::string& socket_path) try {
+    // Every Viam C++ SDK program must have one and only one Instance object which is created before
+    // any other C++ SDK objects and stays alive until all Viam C++ SDK objects are destroyed.
+    viam::sdk::Instance inst;
 
-        // Create a new model registration for the service.
-        auto module_registration = std::make_shared<viam::sdk::ModelRegistration>(
-            // Identify that this resource offers the MLModelService API
-            viam::sdk::API::get<viam::sdk::MLModelService>(),
+    // Create a new model registration for the service.
+    auto module_registration = std::make_shared<viam::sdk::ModelRegistration>(
+        // Identify that this resource offers the MLModelService API
+        viam::sdk::API::get<viam::sdk::MLModelService>(),
 
-            // Declare a model triple for this service.
-            viam::sdk::Model{"viam", "mlmodel-tflite", "tflite_cpu"},
+        // Declare a model triple for this service.
+        viam::sdk::Model{"viam", "mlmodel-tflite", "tflite_cpu"},
 
-            // Define the factory for instances of the resource.
-            [](viam::sdk::Dependencies deps, viam::sdk::ResourceConfig config)
-            {
-                return std::make_shared<mlmodel_tflite::MLModelServiceTFLite>(std::move(deps), std::move(config));
-            });
+        // Define the factory for instances of the resource.
+        [](viam::sdk::Dependencies deps, viam::sdk::ResourceConfig config) {
+            return std::make_shared<mlmodel_tflite::MLModelServiceTFLite>(std::move(deps),
+                                                                          std::move(config));
+        });
 
-        // Register the newly created registration with the Registry.
-        viam::sdk::Registry::get().register_model(module_registration);
+    // Register the newly created registration with the Registry.
+    viam::sdk::Registry::get().register_model(module_registration);
 
-        // Construct the module service and tell it where to place the socket path.
-        auto module_service = std::make_shared<viam::sdk::ModuleService>(socket_path);
+    // Construct the module service and tell it where to place the socket path.
+    auto module_service = std::make_shared<viam::sdk::ModuleService>(socket_path);
 
-        // Add the server as providing the API and model declared in the
-        // registration.
-        module_service->add_model_from_registry(module_registration->api(),
-                                                module_registration->model());
+    // Add the server as providing the API and model declared in the
+    // registration.
+    module_service->add_model_from_registry(module_registration->api(),
+                                            module_registration->model());
 
-        // Start the module service.
-        module_service->serve();
+    // Start the module service.
+    module_service->serve();
 
-        return EXIT_SUCCESS;
-    }
-    catch (const std::exception &ex)
-    {
-        std::cout << "ERROR: A std::exception was thrown from `serve`: " << ex.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    catch (...)
-    {
-        std::cout << "ERROR: An unknown exception was thrown from `serve`" << std::endl;
-        return EXIT_FAILURE;
-    }
+    return EXIT_SUCCESS;
+} catch (const std::exception& ex) {
+    std::cout << "ERROR: A std::exception was thrown from `serve`: " << ex.what() << std::endl;
+    return EXIT_FAILURE;
+} catch (...) {
+    std::cout << "ERROR: An unknown exception was thrown from `serve`" << std::endl;
+    return EXIT_FAILURE;
 }
+}  // namespace
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     const std::string usage = "usage: mlmodelservice_tflite /path/to/unix/socket";
 
-    if (argc < 2)
-    {
+    if (argc < 2) {
         std::cout << "ERROR: insufficient arguments\n";
         std::cout << usage << "\n";
         return EXIT_FAILURE;
--- src/tflite_cpu.hpp
+++ src/tflite_cpu.hpp
@@ -20,90 +20,88 @@
 #include <viam/sdk/rpc/server.hpp>
 #include <viam/sdk/services/mlmodel.hpp>
 
-namespace mlmodel_tflite
-{
+namespace mlmodel_tflite {
 
-    constexpr char service_name[] = "viam_tflite_cpu";
+constexpr char service_name[] = "viam_tflite_cpu";
 
-    // An MLModelService instance which runs TensorFlow Lite models.
-    //
-    // Configuration requires the following parameters:
-    //   -- `model_path`: An absolute filesystem path to a TensorFlow Lite model file.
-    //
-    // The following optional parameters are honored:
-    //   -- `num_threads`: Sets the number of threads to be used, where applicable.
-    //
-    //   -- `label_path`:  An absolute filesystem path to a .txt file of the model's category labels.
-    //
-    // Any additional configuration fields are ignored.
-    class MLModelServiceTFLite : public viam::sdk::MLModelService,
-                                 public viam::sdk::Stoppable,
-                                 public viam::sdk::Reconfigurable
-    {
-        class write_to_tflite_tensor_visitor_;
+// An MLModelService instance which runs TensorFlow Lite models.
+//
+// Configuration requires the following parameters:
+//   -- `model_path`: An absolute filesystem path to a TensorFlow Lite model file.
+//
+// The following optional parameters are honored:
+//   -- `num_threads`: Sets the number of threads to be used, where applicable.
+//
+//   -- `label_path`:  An absolute filesystem path to a .txt file of the model's category labels.
+//
+// Any additional configuration fields are ignored.
+class MLModelServiceTFLite : public viam::sdk::MLModelService,
+                             public viam::sdk::Stoppable,
+                             public viam::sdk::Reconfigurable {
+    class write_to_tflite_tensor_visitor_;
 
-    public:
-        MLModelServiceTFLite(viam::sdk::Dependencies dependencies, viam::sdk::ResourceConfig configuration);
+   public:
+    MLModelServiceTFLite(viam::sdk::Dependencies dependencies,
+                         viam::sdk::ResourceConfig configuration);
 
-        ~MLModelServiceTFLite() final;
+    ~MLModelServiceTFLite() final;
 
-        void stop(const viam::sdk::ProtoStruct &extra) noexcept final;
+    void stop(const viam::sdk::ProtoStruct& extra) noexcept final;
 
-        /// @brief Stops the MLModelServiceTFLite from running.
-        void stop() noexcept;
+    /// @brief Stops the MLModelServiceTFLite from running.
+    void stop() noexcept;
 
-        void reconfigure(const viam::sdk::Dependencies &dependencies,
-                         const viam::sdk::ResourceConfig &configuration) final;
+    void reconfigure(const viam::sdk::Dependencies& dependencies,
+                     const viam::sdk::ResourceConfig& configuration) final;
 
-        std::shared_ptr<named_tensor_views> infer(const named_tensor_views &inputs,
-                                                  const viam::sdk::ProtoStruct &extra) final;
+    std::shared_ptr<named_tensor_views> infer(const named_tensor_views& inputs,
+                                              const viam::sdk::ProtoStruct& extra) final;
 
-        struct metadata metadata(const ::viam::sdk::ProtoStruct &extra) final;
+    struct metadata metadata(const ::viam::sdk::ProtoStruct& extra) final;
 
-    private:
-        struct state_;
+   private:
+    struct state_;
 
-        void check_stopped_inlock_() const;
+    void check_stopped_inlock_() const;
 
-        static std::unique_ptr<struct state_> configure_(viam::sdk::Dependencies dependencies,
-                                                         viam::sdk::ResourceConfig configuration);
+    static std::unique_ptr<struct state_> configure_(viam::sdk::Dependencies dependencies,
+                                                     viam::sdk::ResourceConfig configuration);
 
-        static MLModelService::tensor_info::data_types service_data_type_from_tflite_data_type_(
-            TfLiteType type);
+    static MLModelService::tensor_info::data_types service_data_type_from_tflite_data_type_(
+        TfLiteType type);
 
-        // All of the meaningful internal state of the service is held in
-        // a separate state object to help ensure clean replacement of our
-        // internals during reconfiguration.
-        // struct state_ final : public tflite::ErrorReporter;
+    // All of the meaningful internal state of the service is held in
+    // a separate state object to help ensure clean replacement of our
+    // internals during reconfiguration.
+    // struct state_ final : public tflite::ErrorReporter;
 
-        // A visitor that can populate a TFLiteTensor given a MLModelService::tensor_view.
-        class write_to_tflite_tensor_visitor_ : public boost::static_visitor<TfLiteStatus>
-        {
-        public:
-                       write_to_tflite_tensor_visitor_(const std::string *name, TfLiteTensor *tflite_tensor);
-
-            template <typename T>
-            TfLiteStatus operator()(const T &mlmodel_tensor) const;
-
-        private:
-            const std::string *name_;
-            TfLiteTensor *tflite_tensor_;
-        };
-
-        MLModelService::tensor_views make_tensor_view_(const MLModelService::tensor_info &info,
-                                                       const TfLiteTensor *const tflite_tensor);
+    // A visitor that can populate a TFLiteTensor given a MLModelService::tensor_view.
+    class write_to_tflite_tensor_visitor_ : public boost::static_visitor<TfLiteStatus> {
+       public:
+        write_to_tflite_tensor_visitor_(const std::string* name, TfLiteTensor* tflite_tensor);
 
         template <typename T>
-        MLModelService::tensor_views make_tensor_view_t_(const MLModelService::tensor_info &info,
-                                                         const TfLiteTensor *const tflite_tensor);
+        TfLiteStatus operator()(const T& mlmodel_tensor) const;
 
-        // Accesss to the module state is serialized. All configuration
-        // state is held in the `state` type to make it easier to destroy
-        // the current state and replace it with a new one.
-        std::shared_mutex state_rwmutex_;
-
-        // In C++17, this could be `std::optional`.
-        std::unique_ptr<struct state_> state_;
+       private:
+        const std::string* name_;
+        TfLiteTensor* tflite_tensor_;
     };
 
-} // namespace
+    MLModelService::tensor_views make_tensor_view_(const MLModelService::tensor_info& info,
+                                                   const TfLiteTensor* const tflite_tensor);
+
+    template <typename T>
+    MLModelService::tensor_views make_tensor_view_t_(const MLModelService::tensor_info& info,
+                                                     const TfLiteTensor* const tflite_tensor);
+
+    // Accesss to the module state is serialized. All configuration
+    // state is held in the `state` type to make it easier to destroy
+    // the current state and replace it with a new one.
+    std::shared_mutex state_rwmutex_;
+
+    // In C++17, this could be `std::optional`.
+    std::unique_ptr<struct state_> state_;
+};
+
+}  // namespace mlmodel_tflite
